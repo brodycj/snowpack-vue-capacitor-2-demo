42dbc873319de4280e6c05d1dba5f475a3457f62
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-08-26 12:47:38 -0400

    update comments in scripts/patch-build.sh

--- scripts/patch-build.sh
+++ scripts/patch-build.sh
@@ -1,2 +1,6 @@
-# TBD is this even correct?
+# Quick hack to run on Android
+# (as a separate script due to issues with JSON)
+# TBD not so well tested at this point
+# ref:
+# https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
 npx replace '\?\?' '||' build/web_modules/vue.js
